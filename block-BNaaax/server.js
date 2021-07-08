d1e136dddcd5deb5b8b11a64249b89c110b4d74b
vivek sharma <vivekslim@gmail.com>
2021-07-08 15:56:10 +0530

    block2

---
+++ block-BNaaax/server.js
@@ -0,0 +1,10 @@
+var path = require("path")
+
+
+
+ var absolutePath =__dirname
+ var serverPath=path.join(__dirname, "server.js")
+
+// console.log(absolutePath+"/form.html")  // Absolute path of form.html
+
+console.log(serverPath)  
